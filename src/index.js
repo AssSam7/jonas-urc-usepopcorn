a61a4a699eb6dd6d7b8a6a3cc725d69eb082a5b1
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2023-08-13 20:46:30 +0530

    Adding more meaningful props

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/*     <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      className="active"
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
   </React.StrictMode>
 );
